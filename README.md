4da649fba23f3d8809f2f397193434e7e4a67274
Danilo-Gama <danilorgama@hotmail.com>
2023-04-05 13:41:09 -0300

    Final Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+              DESAFIO: Criando um projeto BOOTCAMP com POO
+
+     A INSPIRAÇÃO PARA ESTE DESAFIO FOI UM MÓDULO DO BOOTCAMP JAVA-DIO,
+     "MÓDULO: CONHECENDO A LINGUAGEM DE PROGRAMAÇÃO JAVA"
+    
+     ISOLANDO E O REPRESENTANDO EM CADA CURSO, DESCRIÇÃO E CONTEÚDOS,
+     ATÉ SUA CONCLUSÃO, GANHANDO UMA PONTUAÇÃO CHAMADA DE XP.
+
+     Neste desafio criei um projeto chamado BOOTCAMP na linguagem JAVA 
+     usando PARADIGMA DE ORIENTAÇÃO A OBJETOS.
+
+     O objetivo principal é colocar em prática umas das principais ferramentas da POO: 
+     ABSTRAÇÃO, ENCAPSULAMENTO, HERANÇA E POLIMORFISMO.
+
+     1. ABSTRAIR o DOMÍNIO Bootcamp e MODELAR seus ATRIBUTOS E MÉTODOS.
+     2. Criaremos as CLASSES: Bootcamp, Cursos, Mentorias e Devs e vamos relacioná-las.
+     3. As CLASSES Curso, Mentoria e Devs também serão MODELADOS, 
+        ou seja, criaremos seus ATRIBUTOS E MÉTODOS.
+     4. Para que o código fique mais legível e de fácil manutenção, iremos utilizar de algumas das ferramentas que o PARADIGMA DE ORIENTAÇÃO A OBJETOS (POO) nos oferece:
+        ABSTRAÇÃO, ENCAPSULAMENTO, HERANÇA E POLIMORFISMO 
+     5. E para representar CLASSES que foram criados e relacionados, 
+        vamos transformar-las em OBJETOS.
--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -1,12 +1,134 @@
 package br.com.dio.desafio.dominio;
 
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("BEM VINDO AO BOOTCAMP JAVA/DIO! ");
+        System.out.println();
+        System.out.println("MÓDULO 1: CONHECENDO A LINGUAGEM DE PROGRAMAÇÃO JAVA");
+        System.out.println();
+
         Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
+        curso1.setTitulo("Aprendendo a sintaxe JAVA");
+        curso1.setDescricao("Conhecendo a estrutura java");
+        curso1.setCargaHoraria(5);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Lógica JAVA");
+        curso2.setDescricao("Conhecendo lógica em Java");
+        curso2.setCargaHoraria(2);
+
+        Curso curso3 = new Curso();
+        curso3.setTitulo("Estruturas de dados JAVA");
+        curso3.setDescricao("conhecendo as estruturas de dados JAVA");
+        curso3.setCargaHoraria(9);
+
+        Curso curso4 = new Curso();
+        curso4.setTitulo("Métodos JAVA");
+        curso4.setDescricao("conhecendo os metodos em Java");
+        curso4.setCargaHoraria(2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Piramide de testes e TDD");
+        mentoria1.setDescricao("aprendendo a realizar testes");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Solid e boas práticas");
+        mentoria2.setDescricao("aprendendo sobre solid e boas práticas");
+        mentoria2.setData(LocalDate.now());
 
         System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(curso3);
+        System.out.println(curso4);
 
+        Bootcamp bootcampJava = new Bootcamp();
+        bootcampJava.setNome("BOOTCAMP JAVA/DIO");
+        bootcampJava.setDescricao("IMERSÃO NO MUNDO JAVA, UM SALTO NA SUA CARREIRA DE DEV.");
+        bootcampJava.getConteudos().add(curso1);
+        bootcampJava.getConteudos().add(mentoria1);
+        bootcampJava.getConteudos().add(curso2);
+        bootcampJava.getConteudos().add(curso3);
+        bootcampJava.getConteudos().add(mentoria2);
+        bootcampJava.getConteudos().add(curso4);
+
+        Dev devCarlos = new Dev();
+        devCarlos.setNome("Carlos");
+        devCarlos.inscreverBootcamp(bootcampJava);
+        System.out.println("Aluno(a):" + devCarlos.getNome());
+        System.out.println("Cursos deste aluno:" + devCarlos.getConteudosInscritos());
+        devCarlos.progredir();
+        devCarlos.progredir();
+        System.out.println("Parabens, você está avancando no BOOTCAMP!");
+
+        System.out.println("Cursos deste aluno:" + devCarlos.getConteudosInscritos());
+        System.out.println("Cursos concluidos:" + devCarlos.getConteudosConcluidos());
+        System.out.println("Total XP:" + devCarlos.calcularTotalXp());
+
+        System.out.println("===============================================================");
+
+        Dev devMaria = new Dev();
+        devMaria.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcampJava);
+        System.out.println("Aluno(a):" + devMaria.getNome());
+        System.out.println("Cursos deste aluno:" + devMaria.getConteudosInscritos());
+        devMaria.progredir();
+        devMaria.progredir();
+        devMaria.progredir();
+        devMaria.progredir();
+        System.out.println("Parabens, você está avancando no BOOTCAMP!");
+
+        System.out.println("Cursos deste aluno:" + devMaria.getConteudosInscritos());
+        System.out.println("Cursos concluidos:" + devMaria.getConteudosConcluidos());
+        System.out.println("Total XP:" + devMaria.calcularTotalXp());
+
+        System.out.println("===============================================================");
+
+        Dev devGama = new Dev();
+        devGama.setNome("Gama");
+        devGama.inscreverBootcamp(bootcampJava);
+        System.out.println("Aluno(a):" + devGama.getNome());
+        System.out.println("Cursos deste aluno:" + devGama.getConteudosInscritos());
+        devGama.progredir();
+        devGama.progredir();
+        devGama.progredir();
+        devGama.progredir();
+        devGama.progredir();
+        System.out.println("Parabens, você está avancando no BOOTCAMP!");
+
+        System.out.println("Cursos deste aluno:" + devGama.getConteudosInscritos());
+        System.out.println("Cursos concluidos:" + devGama.getConteudosConcluidos());
+        System.out.println("Total XP:" + devGama.calcularTotalXp());
+
+        System.out.println("===============================================================");
+
+        Dev devDanilo = new Dev();
+        devDanilo.setNome("Danilo");
+        devDanilo.inscreverBootcamp(bootcampJava);
+        System.out.println("Aluno(a):" + devDanilo.getNome());
+        System.out.println("Cursos deste aluno:" + devDanilo.getConteudosInscritos());
+        devDanilo.progredir();
+        devDanilo.progredir();
+        devDanilo.progredir();
+        devDanilo.progredir();
+        devDanilo.progredir();
+        devDanilo.progredir();
+        System.out.println("Parabens, você está avancando no BOOTCAMP!");
+
+        System.out.println("Cursos deste aluno:" + devDanilo.getConteudosInscritos());
+        System.out.println("Cursos concluidos:" + devDanilo.getConteudosConcluidos());
+        System.out.println("Total XP:" + devDanilo.calcularTotalXp());
+
+        System.out.println("PARABENS! VOCÊ CONCLUIU O BOOTCAMP!");
 
     }
+
 }
